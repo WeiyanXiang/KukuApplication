92bda8c7c718b517b04bb4e8e3758b58e6993293
WeiyanXiang <ltxfxwy@gmail.com>
2022-03-20 21:46:30 +0000

    remove oauth initially

--- src/main/kotlin/com/kuku/KukuApplication.kt
+++ src/main/kotlin/com/kuku/KukuApplication.kt
@@ -2,41 +2,20 @@ package com.kuku
 
 import com.kuku.formats.JacksonMessage
 import com.kuku.formats.jacksonMessageLens
-import org.http4k.client.JavaHttpClient
-import org.http4k.core.Credentials
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
-import org.http4k.core.Uri
 import org.http4k.core.then
 import org.http4k.core.with
 import org.http4k.filter.DebuggingFilters.PrintRequest
 import org.http4k.routing.bind
 import org.http4k.routing.routes
-import org.http4k.security.InsecureCookieBasedOAuthPersistence
-import org.http4k.security.OAuthProvider
-import org.http4k.security.google
 import org.http4k.server.ApacheServer
 import org.http4k.server.asServer
 
-// Google OAuth Example
-// Browse to: http://localhost:9000/oauth - you'll be redirected to google for authentication
-val googleClientId = "myGoogleClientId"
-val googleClientSecret = "myGoogleClientSecret"
-
-// this is a test implementation of the OAuthPersistence interface, which should be
-// implemented by application developers
-val oAuthPersistence = InsecureCookieBasedOAuthPersistence("Google")
-
-// pre-defined configuration exist for common OAuth providers
-val oauthProvider = OAuthProvider.google(
-        JavaHttpClient(),
-        Credentials(googleClientId, googleClientSecret),
-        Uri.of("http://localhost:9000/oauth/callback"),
-        oAuthPersistence
-)
 val app: HttpHandler = routes(
+
     "/ping" bind GET to {
         Response(OK).body("pong")
     },
@@ -45,14 +24,11 @@ val app: HttpHandler = routes(
         Response(OK).with(jacksonMessageLens of JacksonMessage("Barry", "Hello there!"))
     },
 
-    "/testing/hamkrest" bind GET to {request ->
+    "/testing/hamkrest" bind GET to { request ->
         Response(OK).body("Echo '${request.bodyString()}'")
-    },
+    }
 
-    "/oauth" bind routes(
-            "/" bind GET to oauthProvider.authFilter.then { Response(OK).body("hello!") },
-            "/callback" bind GET to oauthProvider.callback
-    )
+
 )
 
 fun main() {
